8e1277cfc9e5e89b21d0e0d318a3cb8e3ace225c
Cheng <62643483+chhnet@users.noreply.github.com>
2020-07-03 18:54:17 +0800

    update

---
+++ config/MyDataSourceConfig.java
@@ -0,0 +1,17 @@
+package com.chhnet.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MyDataSourceConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource(){
+        return new DruidDataSource();
+    }
+}
