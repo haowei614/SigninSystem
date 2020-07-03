dd709cd54e049e7227e90b5cee987e97ca8dee79
Cheng <62643483+chhnet@users.noreply.github.com>
2020-07-03 18:59:42 +0800

    update

---
+++ SinginManagementSystemApplication.java
@@ -0,0 +1,11 @@
+package com.chhnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinginManagementSystemApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SinginManagementSystemApplication.class,args);
+    }
+}
